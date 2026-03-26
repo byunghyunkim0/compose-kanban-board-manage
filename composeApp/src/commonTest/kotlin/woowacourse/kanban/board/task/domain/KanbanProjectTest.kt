35661992023304c3d998bd073e9387cbfcdb65c0
byunghyunkim0 <bb05qudgus@gamil.com>
2026-03-26 13:46:59 +0900

    feat: KanbanProject 로직 구현

--- composeApp/src/commonTest/kotlin/woowacourse/kanban/board/task/domain/KanbanProjectTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/task/domain/KanbanProjectTest.kt
@@ -6,25 +6,33 @@ import org.assertj.core.api.Assertions.assertThat
 class KanbanProjectTest {
     @Test
     fun `boardId에 따라 KanbanCard를 분류한다`() {
-        val kanbanProject = KanbanProject(
-            projectTitle = "칸반 프로젝트",
-            kanbanCards = listOf(
-                createKanbanCard(
-                    cardId = 0,
-                    boardId = 0,
-                ),
-                createKanbanCard(
-                    cardId = 1,
-                    boardId = 1,
-                ),
+        val kanbanProject = KanbanProject("프로젝트")
+
+        val newKanbanProject = kanbanProject.addCard(
+            createKanbanCard(
+                0,
+                0,
+                status = KanbanStatus.IN_PROGRESS,
+            ),
+        ).addCard(
+            createKanbanCard(
+                1,
+                0,
+                status = KanbanStatus.DONE,
+            ),
+        ).addCard(
+            createKanbanCard(
+                2,
+                1,
+                status = KanbanStatus.DONE,
             ),
         )
 
-        val boardIdList = kanbanProject.getKanbanCardByBoardId(0)
-        val boardIdList1 = kanbanProject.getKanbanCardByBoardId(1)
+        val boardIdList = newKanbanProject.getKanbanCardByBoardId(0)
+        val boardIdList1 = newKanbanProject.getKanbanCardByBoardId(1)
 
-        assertThat(boardIdList.size).isEqualTo(1)
-        assertThat(boardIdList1.size).isEqualTo(0)
+        assertThat(boardIdList.size).isEqualTo(2)
+        assertThat(boardIdList1.size).isEqualTo(1)
     }
 
     @Test
@@ -72,7 +80,7 @@ class KanbanProjectTest {
             status = KanbanStatus.TO_DO,
         )
 
-        assertThat(updateKanbanProject.getKanbanCard(1).status).isEqualTo(KanbanStatus.TO_DO)
+        assertThat(updateKanbanProject.getKanbanCard(1)?.status).isEqualTo(KanbanStatus.TO_DO)
     }
 
     private fun createKanbanCard(cardId: Long, boardId: Int, status: KanbanStatus = KanbanStatus.TO_DO) = KanbanCard(
